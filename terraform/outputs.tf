f4e7edfea072b76f8b665d765ad5ba957893b8e4
Danny O'Brien <danny.obrien@aicadium.ai>
2023-06-14 10:44:54 -0700

    remove terraform files

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "container_id" {
-  description = "ID of the Docker container"
-  value       = docker_container.this.id
-}
-
-output "image_id" {
-  description = "ID of the Docker image"
-  value       = docker_image.this.image_id
-}
